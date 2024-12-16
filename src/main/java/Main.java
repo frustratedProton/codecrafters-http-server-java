bd0dffc6244a9e2ec51066f1d6369e53e75783d5
Proton <aryan29052003@gmail.com>
2024-12-16 12:46:45 +0530

    Added - Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,8 +17,14 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       Socket clinetSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       // need to add outputstream to respond with HTTP status code
+         // The \r\n\r\n separates HTTP headers from the body, with the first \r\n ending
+         // the last header and the second \r\n indicating the start of the body.
+       String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+       clinetSocket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
